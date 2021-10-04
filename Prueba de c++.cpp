f9928a20a46a546480bfcfa7497c4a8221e1f335
Nelver <nelverlinux@gmail.com>
2021-10-04 02:14:55 -0600

    Desde linux

--- Prueba de c++.cpp
+++
@@ -1,7 +0,0 @@
-include<iostream>
-include<conio.h>
-usingnamespace std;
-int main(){
-    cout<<"Hola mundo";
-    
-}
---
+++ practicaprofesional/android/app/src/main/kotlin/com/example/practicaprofesional/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.practicaprofesional
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
