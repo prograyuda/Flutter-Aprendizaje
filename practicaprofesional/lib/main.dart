fdeec160ddec4011a2606469552e1c9280e9e0b1
prograyuda <72632824+prograyuda@users.noreply.github.com>
2021-10-04 18:32:51 -0600

    dasda

--- practicaprofesional/lib/main.dart
+++ practicaprofesional/lib/main.dart
@@ -1,2 +1,23 @@
+
 import 'package:flutter/material.dart';
-1
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Container(
+            child: Text('Hello World'),
+          ),
+        ),
+      ),
+    );
+  }
+}
